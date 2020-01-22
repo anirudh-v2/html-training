ccb738b260adcebb200ecad173ee38bb24baf68d
anirudh-v2 <anirudh.p@v2solution.com>
2020-01-22 10:29:18 +0530

    remove additional variable

--- distinct-elements.js
+++ distinct-elements.js
@@ -1,18 +1,13 @@
 var array = [10, 3, 5, 12, 5, 3, 9, 1];
-var element_count = [];
 for (i = 0; i < array.length; i++) {
     count = 1;
     for (j = i + 1; j < array.length; j++) {
         if (array[i] == array[j]) {
             count++;
-            element_count[j] = 0
+            array[j] = 0
         }
     }
-    if (element_count[i] != 0) {
-        element_count[i] = count;
+    if (array[i] != 0) {
+        console.log(array[i] + "-" + count);
     }
-}
-for (var i = 0; i < array.length; i++) {
-    if (element_count[i] != 0)
-        console.log(array[i] + "-" + element_count[i]);
 }
