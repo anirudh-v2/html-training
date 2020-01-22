9464f06cd5615525056b4f27f88ecbb8fcc15457
anirudh-v2 <anirudh.p@v2solution.com>
2020-01-22 10:14:09 +0530

    update multiple occurance of element

--- distinct-elements.js
+++ distinct-elements.js
@@ -1,11 +1,18 @@
 var array = [10, 3, 5, 12, 5, 3, 9, 1];
-
+var element_count = [];
 for (i = 0; i < array.length; i++) {
-    count = 0;
-    for (j = 0; j < array.length; j++) {
+    count = 1;
+    for (j = i + 1; j < array.length; j++) {
         if (array[i] == array[j]) {
             count++;
+            element_count[j] = 0
         }
     }
-    console.log(array[i] + "-" + count);
+    if (element_count[i] != 0) {
+        element_count[i] = count;
+    }
+}
+for (var i = 0; i < array.length; i++) {
+    if (element_count[i] != 0)
+        console.log(array[i] + "-" + element_count[i]);
 }
