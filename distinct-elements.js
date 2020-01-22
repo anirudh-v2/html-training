de97da31683a99f389772a3121498661b4f117a6
anirudh-v2 <anirudh.p@v2solution.com>
2020-01-22 17:33:57 +0530

    update occurances of element in array

--- distinct-elements.js
+++ distinct-elements.js
@@ -1,13 +1,13 @@
-var array = [10, 3, 5, 12, 5, 3, 9, 1];
+var array = [0 ,5 ,10, 3, 5, 12, 5, 3, 9, 1 ,0, 5];
 for (i = 0; i < array.length; i++) {
     count = 1;
     for (j = i + 1; j < array.length; j++) {
         if (array[i] == array[j]) {
             count++;
-            array[j] = 0
+            array[j] = null
         }
     }
-    if (array[i] != 0) {
+    if (array[i] != null) {
         console.log(array[i] + "-" + count);
     }
 }
